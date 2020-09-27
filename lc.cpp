0b5dc2e22374131130321c123d19541b85deebd0
Dipesh Singh <58405253+singhdipesh14@users.noreply.github.com>
2020-09-27 00:15:37 +0000

    subah ke 6 baj rahe hai

--- lc.cpp
+++ lc.cpp
@@ -3,39 +3,54 @@ using namespace std;
 
 class Solution {
 public:
-    
-    string rec(string n){
-        int cnt = 0;
-        string val = "";
-        char c = n[0];
-        for(int i = 0; i< n.size(); i++){
-            if(c == n[i]){
-                cnt++;
+    string addBinary(string a, string b) {
+        int lena = a.size()-1;
+        int lenb = b.size()-1;
+        int carry = 0;
+        string res = "";
+        while(1){
+            int aa, bb;
+            if(lena < 0 && lenb < 0){
+                break;
+            }
+            
+            if(lena < 0){
+                aa = 0;
             }
             else{
-                val+=(char)(cnt+'0');
-                val+=c;
-                cnt = 0;
-                c = n[i];
-                i--;
+                aa = (int)(a[lena] - '0');
             }
+            if(lenb < 0){
+                bb = 0;
+            }
+            else{
+                bb = (int)(b[lenb] - '0');
+            }
+            int sum = aa + bb + carry;
+            carry = sum / 2;
+            int add = sum % 2;
+            cout << aa << '\t' << bb << '\t' << add << '\t'<< carry << endl;
+            res+=to_string(add);
+            lena--;
+            lenb--;
         }
-        return val;
-    }
-    
-    string countAndSay(int n) {
-        string base = "1"
-        for(int i = 1; i<=n; i++){
-            base = rec(base);
+        if(carry){
+            cout << "adding final" << endl;
+            res+='1';
         }
-        return base;
+        string ans;
+        for(int i = res.size()-1; i>=0;i--){
+            //cout << res[i] << endl;
+            ans+=res[i];
+        }
+        return ans;
     }
 };
 
 int main()
 {
     Solution sol;
-    string ans = sol.countAndSay(4);
+    string ans = sol.addBinary("0", "0");
     cout << ans << endl;
     return 0;
 }
